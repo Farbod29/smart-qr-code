e44eabbfd194be9163fd61c9366111aae3c15b1a
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2018-06-24 17:36:49 +0200

    start with addLink

---
+++ src/components/AddLink/AddLink.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AddLink extends Component {
+    render() {
+        return (
+            <div className="App">
+
+            </div>
+        );
+    }
+}
+
+export default AddLink;
