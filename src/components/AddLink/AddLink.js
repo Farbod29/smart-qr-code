3ffeb308f6b5a3f205e1949e98ea72b41e9ff53f
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2018-06-28 00:56:32 +0200

    test pushing without type my crid.

--- src/components/AddLink/AddLink.js
+++ src/components/AddLink/AddLink.js
@@ -70,7 +70,7 @@ class AddLink extends Component {
                             <br/>
                         </div>
                     </form>
-                </div>
+                </div> 
             </div>
         );
     }
