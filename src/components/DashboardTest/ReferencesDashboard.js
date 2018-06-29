e795351a6ad5ee34eb00470bdc18059c10d0495b
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2018-06-29 16:51:42 +0200

    new implementation for a card using mercury api -not finished

--- src/components/DashboardTest/ReferencesDashboard.js
+++ src/components/DashboardTest/ReferencesDashboard.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Header from "../Header/Header";
 import ReferenceCard from "./ReferenceCard";
+import ResourceCard from "./ResourceCard";
 
 class ReferencesDashboard extends Component {
     render() {
@@ -10,13 +11,22 @@ class ReferencesDashboard extends Component {
 
                 <Header/>
 
-                <div className="row">
-                    <ReferenceCard url="https://www.youtube.com/watch?v=5jVnLbdqR6U"/>
-                    <ReferenceCard url="https://www.youtube.com/watch?v=KMX1mFEmM3E"/>
-                    <ReferenceCard url="https://www.youtube.com/watch?v=oa9cnWTpqP8"/>
-                    <ReferenceCard url="https://www.youtube.com/watch?v=78tNYZUS-ps"/>
-                </div>
+                {/*<div className="row">*/}
+                {/*<ReferenceCard url="https://www.youtube.com/watch?v=5jVnLbdqR6U"/>*/}
+                {/*<ReferenceCard url="https://www.youtube.com/watch?v=KMX1mFEmM3E"/>*/}
+                {/*<ReferenceCard url="https://www.youtube.com/watch?v=oa9cnWTpqP8"/>*/}
+                {/*</div>*/}
 
+                <div className="row mt-5 row-centered" id="card-container">
+                    <ResourceCard url="https://github.com/facebook/react"/>
+                    <ResourceCard url="https://www.youtube.com/watch?v=KMX1mFEmM3E"/>
+                    <ResourceCard url="https://material-ui.com/demos/cards/#cards"/>
+                    <ResourceCard url="https://www.android.com/"/>
+                    <ResourceCard url="https://vuejs.org/"/>
+                    <ResourceCard url="https://jsoneditoronline.org/"/>
+                    <ResourceCard url="https://bitbucket.org/"/>
+                    <ResourceCard url="https://moodle.uni-due.de"/>
+                </div>
 
             </div>
         );
