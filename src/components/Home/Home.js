4ed298410b843262a2d21ac43434a361dd581ef5
yazan <yazan.zaeem@hotmail.com>
2018-07-10 11:14:42 -0700

    QRcode generator

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,9 @@ import Button from "@material-ui/core/es/Button/Button";
 import TextField from "@material-ui/core/es/TextField/TextField";
 import Header from "../Header/Header";
 import {createNewBoardData} from "../../utils/Connection";
+import QRCode from "qrcode-react";
 
+import ReactDOM from "react-dom";
 
 class Home extends Component {
 
@@ -18,7 +20,8 @@ class Home extends Component {
 
         this.state = {
             title: "",
-            boardLink: ""
+            boardLink: "",
+            QRcode: ""
         };
     }
 
@@ -48,43 +51,41 @@ class Home extends Component {
     }
 
     render() {
-        return (
-            <div className="App">
+        return <div className="App">
 
 
-                <Header/>
-                <div className="login-clean">
-                    <form method="post"
-                          onSubmit={this.createNewBoard.bind(this)}>
-                        <h2>Create New Board</h2>
+            <Header/>
+            <div className="login-clean">
+                <form method="post"
+                      onSubmit={this.createNewBoard.bind(this)}>
+                    <h2>Create New Board</h2>
 
-                        <div className="form-group">
-                            <TextField
-                                id="title"
-                                label="title"
-                                placeholder="type board title"
-                                name="title"
-                                value={this.state.email}
-                                onChange={this.handleChange}
-                                margin="normal"
-                                className="form-control"/>
-                        </div>
+                    <div className="form-group">
+                        <TextField
+                            id="title"
+                            label="title"
+                            placeholder="type board title"
+                            name="title"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            margin="normal"
+                            className="form-control"/>
+                    </div>
 
-                        <div className="form-group">
-                            <Button variant="contained" color="primary" type="submit" disabled={!this.validateForm()}>
-                                Create
-                            </Button>
-                        </div>
+                    <div className="form-group">
+                        <Button variant="contained" color="primary" type="submit" disabled={!this.validateForm()}>
+                            Create
+                        </Button>
+                    </div>
 
-                        {this.state.boardLink.length > 0 ? (
-                            window.location.href + "board/" + this.state.boardLink
-                            ) : (null)}
-                    </form>
-                </div>
+                    {this.state.boardLink.length > 0 ? (
+                        this.state.QRcode = window.location.href + "board/" + this.state.boardLink
+                    ) : (null)}
 
-
+                    <QRCode value={this.state.QRcode}/>
+                </form>
             </div>
-        );
+        </div>;
     }
 }
 
