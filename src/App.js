d6bed5095996830c4a136d754f1cee97ec11ff12
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2018-07-14 21:30:44 +0200

    remove forgot password, register

--- src/App.js
+++ src/App.js
@@ -6,27 +6,20 @@ import './general.css';
 import Login from "./components/Login/Login";
 import AddLink from "./components/AddLink/AddLink";
 import Settings from "./components/Settings/Settings";
-import Register from "./components/Register/Register";
-import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
+// import Register from "./components/Register/Register";
+// import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 import ReferenceCard from "./components/DashboardTest/ReferenceCard";
 import ReferencesDashboard from "./components/DashboardTest/ReferencesDashboard";
 import Home from "./components/Home/Home";
 
 class App extends Component {
-    state =
-        {
-            // cards:
-            //     [
-            //         {id: '1as3dasde3434', name: 'PDF', age: 131},
-            //     ],
-        };
+
   render() {
       return <div className="App">
           <BrowserRouter>
               <div className="sans-serif">
                   <Route exact path="/" component={Home}/>  {/*this is a main entry .. we will change it later .. but i write it now to don't show a blank page in the start of the project*/}
-                  {/*<Route path="/Home" component={Home}/>*/}
-                  <Route path="/ForgotPassword" component={ForgotPassword}/>
+                  {/*<Route path="/ForgotPassword" component={ForgotPassword}/>*/}
                   <Route path="/DashboardTest" component={ReferenceCard}/>
                   <Route path="/board" component={ReferencesDashboard}/>
                   <Route path="/board?id=:id" component={ReferencesDashboard}/>
@@ -34,7 +27,7 @@ class App extends Component {
                   <Route path="/AddLink" component={AddLink}/>
                   <Route path="/settings" component={Settings}/>
                   {/*<Route path="/emaam" component={Dashboard}/>*/}
-                  <Route path="/Register" component={Register}/>
+                  {/*<Route path="/Register" component={Register}/>*/}
               </div>
           </BrowserRouter>
       </div>;
