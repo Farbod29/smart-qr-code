62f95c8eabb49a5db2ad43da5877955edb872d74
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2018-07-15 01:18:29 +0200

    fix backend endpoint

--- src/utils/StorageKeys.js
+++ src/utils/StorageKeys.js
@@ -3,7 +3,7 @@ class StorageKeys {
     static EMAIL = "email";
     static PHOTO_URL = "photoURL";
 
-    // static BASE_API_URL = "https://smart-learning-backend.herokuapp.com/"
-    static BASE_API_URL = "http://localhost:7000/"
+    static BASE_API_URL = "https://smart-learning-backend.herokuapp.com/"
+    // static BASE_API_URL = "http://localhost:7000/"
 }
 export default StorageKeys;
