131ffc7e7a2ee876467223e5db43e96481639249
yazan <yazan.zaeem@hotmail.com>
2018-07-16 11:18:11 -0700

    QRscanner

--- src/components/qrScanner/QrScanner.js
+++ src/components/qrScanner/QrScanner.js
@@ -3,7 +3,6 @@ import QrReader from 'react-qr-scanner'
 import './QrScanner.css';
 import Header from "../header/Header";
 import Button from "@material-ui/core/es/Button/Button";
-import {LinkContainer} from "react-router-bootstrap";
 
 class QrScanner extends Component {
     constructor(props) {
@@ -14,6 +13,10 @@ class QrScanner extends Component {
         };
 
         this.handleScan = this.handleScan.bind(this)
+    }
+
+    validateForm() {
+        return this.state.result.length > 0 ;
     }
 
     handleError(err) {
@@ -34,29 +37,23 @@ class QrScanner extends Component {
             width: 270,
         };
         return (
-            <div className={"centered"}>
+            <div className={"App container text-center"}>
                 <Header/>
-                <QrReader
-                    className={"previewStyle"}
+                 <br/><br/><br/><br/><br/>
+                 <QrReader
                     delay={this.state.delay}
                     style={previewStyle}
                     onError={this.handleError}
                     onScan={this.handleScan}
-                    //style={{ width: '100%' }}
                 />
-
+                  <br/><br/><br/>
                 <div className="form-group">
                     <a href={this.state.result} style={{ color: '#3267b2' }}>
-                        <Button variant="contained" color="primary" type="submit" >
+                        <Button variant="contained" color="primary" type="submit" disabled={!this.validateForm()}>
                            Open Board
                         </Button>
                     </a>
                 </div>
-                    {/*<Button variant="contained" color="primary" type="submit" disabled={!this.validateForm()}>*/}
-                       {/*Open Board*/}
-                    {/*</Button>*/}
-
-                {/*<p className={"truncate"}> SECTION LINK : </p>*/}
             </div>
         )
     }
